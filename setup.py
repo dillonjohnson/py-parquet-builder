8084ffd5b5d57ad43b7d91246b1096ca27b721b7
Dillon Johnson <djrepec@gmail.com>
2020-04-26 14:34:20 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='py-parquet-builder',
+    version='1.0',
+    packages=['py_parquet_builder'],
+    url='',
+    license='',
+    author='dillonjohnson',
+    author_email='dillonjohnson1015@gmail.com',
+    description='This library creates a parquet from an array of dicts.'
+)
