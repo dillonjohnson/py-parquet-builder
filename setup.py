eef84889cef0e5e805cef6effcc1e977df2aecfb
Dillon Johnson <djrepec@gmail.com>
2020-04-26 15:30:59 -0400

    Add requirements

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
 from distutils.core import setup
+import os
+
+the_lib_folder = os.path.dirname(os.path.realpath(__file__))
+
+requirement_path = the_lib_folder + '/requirements.txt'
+install_requires = []
+if os.path.isfile(requirement_path):
+    with open(requirement_path) as f:
+        install_requires = f.read().splitlines()
 
 setup(
     name='py-parquet-builder',
@@ -8,5 +17,6 @@ setup(
     license='',
     author='dillonjohnson',
     author_email='dillonjohnson1015@gmail.com',
-    description='This library creates a parquet from an array of dicts.'
+    description='This library creates a parquet from an array of dicts.',
+    install_requires=install_requires
 )
